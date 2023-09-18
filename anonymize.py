ddf07e956c50de81b3c29849a1b8cd4b0f6ab4d3
agjackson <agjackson@uchicago.edu>
2023-09-18 13:10:47 -0500

    larger dataset

--- anonymize.py
+++ anonymize.py
@@ -1,22 +1,37 @@
 from presidio_analyzer import AnalyzerEngine
 from presidio_anonymizer import AnonymizerEngine
 
-text="My phone number is 212-555-5555"
+#for the specific list part
+from presidio_analyzer import PatternRecognizer
+
+# redacts PII: credit card numbers, names, locations, phone numbers, SSNs, 
+# and financial data
+
+#titles_list = ["Sir","Ma'am","Madam","Mr.","Mrs.","Ms.","Miss","Dr.","Professor",]
+
+#titles_recognizer = PatternRecognizer(supported_entity="TITLE", deny_list=titles_list)
+
+text="This is Will, he's my son. My name is Jessica. His name is Mr. Jones and his phone number is 212-555-5555. I suspect Professor Plum, in the Dining Room, with the candlestick in Chicago"
+
+
+#result = titles_recognizer.analyze(text, entities=["TITLE"])
+#print(f"Result:\n {result}")
+
 
 # Set up the engine, loads the NLP module (spaCy model by default) 
 # and other PII recognizers
 analyzer = AnalyzerEngine()
+#analyzer.registry.add_recognizer(titles_recognizer)
 
 # Call analyzer to get results
-results = analyzer.analyze(text=text,
-                           entities=["PHONE_NUMBER"],
-                           language='en')
+results = analyzer.analyze(text=text,language='en')
+
 print(results)
 
 # Analyzer results are passed to the AnonymizerEngine for anonymization
 
 anonymizer = AnonymizerEngine()
-
+ 
 anonymized_text = anonymizer.anonymize(text=text,analyzer_results=results)
 
 print(anonymized_text)
